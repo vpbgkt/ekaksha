90fbd1de0646d54448d0707bbc844136a1da9618
Vishal prajapat <96687108+vpbgkt@users.noreply.github.com>
2023-05-29 22:17:47 +0530

    Subject Detail model created

--- ClassName/models.py
+++ ClassName/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class ClassName(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    class_image = models.ImageField(upload_to='uploads/subject_images/', null=True, blank=True)
+    class_image = models.ImageField(upload_to='uploads/class_images/', null=True, blank=True)
     
---
+++ SubjectDetail/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from ClassName.models import ClassName
+
+class Subjects(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    instructure_name = models.CharField(max_length=50)
+    description = models.TextField()
+    class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE,related_name='subjects')
+    subject_image = models.ImageField(upload_to='uploads/subject_images/', null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    
